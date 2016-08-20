3431faae4ea99d747685c843b1d6796660cb2acc
Baspar <blaine@palo-it.com>
2016-08-21 00:27:34 +0800

    [add] Add summary

--- src/template/ui.cljs
+++ src/template/ui.cljs
@@ -2,10 +2,44 @@
   (:require
     [template.dispatch :as dispatch]
     [template.util :as util]
-    [template.pages :as pages :refer [render-page]])
+    [template.pages :as pages])
   (:require-macros
    [devcards.core :as dc :refer [defcard deftest]]
    [sablono.core :as sab :refer [html]]))
+
+(defn render-page
+  ([state page]
+   (html [:div {:key ((util/get-slide state page) :id)
+                :style {:height "100%"
+                        :width "100%"
+                        :position "absolute"
+                        :display "flex"
+                        :flex-direction "column"
+                        :align-items "center"}}
+          [:h1 (:title (util/get-slide state page))]
+          [:div {:style {:position "relative"
+                         :display "flex"
+                         :flex-grow "1"
+                         :flex-direction "column"
+                         :align-items "stretch"
+                         :width "100%"}}
+           (pages/render-page state page)]]))
+  ([state]
+   (html [:div {:key ((util/get-slide state) :id)
+                :style {:height "100%"
+                        :width "100%"
+                        :position "absolute"
+                        :display "flex"
+                        :flex-direction "column"
+                        :align-items "center"}}
+          [:h1 (:title (util/get-slide state))]
+          [:div {:style {:position "relative"
+                         :display "flex"
+                         :flex-grow "1"
+                         :flex-direction "column"
+                         :align-items "stretch"
+                         :width "100%"}}
+           (pages/render-page state)]])))
 
 (def header
   [:svg {:height "20%"
@@ -45,43 +79,88 @@
                                   :style {:width "50vw"
                                           :position "absolute"}}
                             ((util/get-group state) :title)]))])
+(defn summary [state]
+  [:div {:style {:z-index 2
+                 :position "absolute"
+                 :display "flex"
+                 :max-width "100vw"
+                 :max-height "100vh"
+                 :pointer-events (if (get @state :menu-visible false) "auto" "none")
+                 :overflow "hidden"}}
+   [:div {:on-click #(swap! state (fn [x] (-> x
+                                              (dissoc :highlight)
+                                              (assoc :menu-visible false))))
+          :style {:height "100vh"
+                  :width "100vw"
+                  :background-color "rgba(100, 100, 100, 0.5)"
+                  :display "flex"
+                  :opacity (if (get @state :menu-visible false) 1 0)
+                  :transform (str "scale(" (if (get @state :menu-visible false) "1)" "8)"))
+                  :transition "transform .3s ease-in-out,
+                               opacity .3s"
+                  :justify-content "center"
+                  :align-items "center"}}
+    [:div
+     (map-indexed (fn [i1 x]
+                    [:div {:style {:padding "5px"
+                                   :font-size "24px"}}
+                     (:title x)
+                     (map-indexed (fn [i2 y] [:div {:on-mouse-enter #(swap! state assoc :highlight [i1 i2])
+                                                    :on-mouse-leave #(swap! state dissoc :highlight)
+                                                    :on-click #(do (swap! state (fn [x] (-> x
+                                                                                        (dissoc :highlight)
+                                                                                        (assoc :menu-visible false))))
+                                                                   (util/go-to state [i1 i2]))
+                                                    :style {:text-align "center"
+                                                            :margin-left "40px"
+                                                            :padding "5px"
+                                                            :margin-top "5p"
+                                                            :font-size "16px"
+                                                            :opacity (if (= [i1 i2] (@state :highlight)) 1 0.5)
+                                                            :cursor "pointer"
+                                                            :background-color "grey"
+                                                            :border-radius "15px"}}
+                                              (:title y)])
+                                  (:slides x))])
+                  (@state :groups))]
+    [:div {:style {:height "50vh"
+                   :width "50vw"
+                   :transform "scale(.5)"
+                   :background-color "white"
+                   ;:box-shadow "0px 0px 2px white"
+                   :border "solid white 15px"
+                   :opacity (if (@state :highlight) 1 0)
+                   ;:transition "opacity 0.3s"
+                   }}
+     (when-let [page (@state :highlight)]
+       (render-page state page))]]])
 (defn page [state]
   (html
-    [:div {:style {:width "100vw"
-                   :height "100vh"
-                   :position "relative"
-                   :overflow "hidden"
-                   :-webkit-filter (str "blur(" (if (get @state :menu-visible false) 5 0) "px)")
-                   :transition "all 0.5s"
-                   :background-color "white"}}
-     header
-     (title state)
-     [:div {:style {:position "relative"
-                    :perspective "1000px"
-                    :height "80%"
-                    :width "100%"}}
-      (.createElement js/React js/React.addons.CSSTransitionGroup
-                      #js {:transitionName (get @state :anim "next")
-                           :transitionLeave true
-                           :transitionEnter true
-                           :transitionLeaveTimeout 500
-                           :transitionEnterTimeout 500}
-                      (html [:div {:key ((util/get-slide state) :id)
-                                   :style {:height "100%"
-                                           :width "100%"
-                                           :position "absolute"
-                                           :display "flex"
-                                           :flex-direction "column"
-                                           :align-items "center"}}
-                             [:h1 (:title (util/get-slide state))]
-                             [:div {:style {:position "relative"
-                                            :display "flex"
-                                            :flex-grow "1"
-                                            :flex-direction "column"
-                                            :align-items "center"
-                                            :justify-content "space-around"
-                                            :width "100%"}}
-                              (render-page state)]]))]]))
+    [:div
+     (summary state)
+     [:div {:style {:width "100vw"
+                    :height "100vh"
+                    :position "relative"
+                    :overflow "hidden"
+                    :-webkit-filter (str "blur(" (if (get @state :menu-visible false) 5 0) "px)")
+                    :display "flex"
+                    :flex-direction "column"
+                    :align-items "center"
+                    :transition "-webkit-filter 0.5s"
+                    :background-color "white"}}
+      header
+      (title state)
+      [:div {:style {:position "relative"
+                     :perspective "1000px"
+                     :height "75%"
+                     :width "80%"}}
+       (.createElement js/React js/React.addons.CSSTransitionGroup
+                       #js {:transitionName (get @state :anim "next")
+                            :transitionLeave true
+                            :transitionEnter true
+                            :transitionLeaveTimeout 500
+                            :transitionEnterTimeout 500}
+                       (render-page state))]]]))
 
 (defn app [state]
   "Show or transition between application pages"
